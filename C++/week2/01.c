47b4d7eeb0315222ec985399dc1b3ffddaf18a26
Dinlon5566 <67054990+Dinlon5566@users.noreply.github.com>
2021-03-08 13:13:26 +0800

    Update 01.c

--- C++/week2/01.c
+++ C++/week2/01.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+
+char *str_replace (char *source, char *find,  char *rep){
+   int find_L=strlen(find);
+   int rep_L=strlen(rep);
+   int length=strlen(source)+1;
+   int gap=0;
+   char *result = (char*)malloc(sizeof(char) * length);
+   strcpy(result, source);
+   char *former=source;
+   char *location= strstr(former, find);
+
+   while(location!=NULL){
+       gap+=(location - former);
+       result[gap]='\0';
+       length+=(rep_L-find_L);
+       result = (char*)realloc(result, length * sizeof(char));
+       strcat(result, rep);
+       gap+=rep_L;
+
+       former=location+find_L;
+       strcat(result, former);
+       location= strstr(former, find);
+   }
+
+   return result;
+
+}
 int main()
 {
-    int T;
-    scanf("%d\n",&T);
-    char cmd[100];
-    char str[100];
+    char orginText[255],search[255],replace[255];
+    while(gets(search)){
+        if(strcmp(search,"*")==0){
+            break;
+        }
+        gets(replace);
+        gets(orginText);
+        puts(str_replace(orginText,search,replace));
 
-    while(T--){
-        gets(cmd);
-
-        gets(str);
-
-
-        if(strcmp(cmd,"ECHO")==0){
-            printf("%s",str);
-        }else if (strcmp(cmd,"UPPERCASE")==0){
-            for(int i=0;i<strlen(str);i++){
-                printf("%c",toupper(str[i]));
-            }
-        }else if(strcmp(cmd,"LOWERCASE")==0){
-            for(int i=0;i<strlen(str);i++){
-                printf("%c",tolower(str[i]));
-            }
-        }else if(strcmp(cmd,"CAPITALIZATION")==0) {
-            str[0]=toupper(str[0]);
-            for(int i=0;i<strlen(str)-1;i++){
-                if(str[i]==' '){
-                    str[i+1]=toupper(str[i+1]);
-                }
-            }
-            printf("%s",str);
-        }else if()
-
-        puts("");
     }
     return 0;
 }
+
