767b4a5245340f61ad8afbad02e5dc454604ad9d
Dinlon5566 <ton200168@gmail.com>
2023-03-11 15:32:25 +0800

    Add bisectionMethod,del redundancy

--- Numerical analysis/rootOfEqutions.py
+++ Numerical analysis/rootOfEqutions.py
@@ -14,6 +14,7 @@ class Equation:
     def __init__(self) -> None:
         self.functions = [self.f1, self.f2, self.f3]
 
+
 class Solution:
     def falsePostion(self,function,a,b,EPS=0.00001):
         # find the value of f(a),f(b)
@@ -22,10 +23,10 @@ class Solution:
         
         if(Fa*Fb>0):
             raise ValueError("The function has no root in the interval")
-        
-        m,c=self.lineEq(function,a,b)
+
         x1=a+function(a)*(b-a)/(function(a)-function(b))
         Fx1=function(x1)
+
         if(Fx1==0):
             return x1
         elif(abs(Fx1)<EPS):
@@ -34,6 +35,25 @@ class Solution:
             return self.falsePostion(function,a,x1,EPS)
         else:
             return self.falsePostion(function,x1,b,EPS)
+
+    def bisectionMethod(self,function,a,b,EPS=0.00001):
+        # find the value of f(a),f(b)
+        Fa=function(a)
+        Fb=function(b)
+        
+        if(Fa*Fb>0):
+            raise ValueError("The function has no root in the interval")
+        
+        x1=(a+b)/2
+        Fx1=function(x1)
+        if(Fx1==0):
+            return x1
+        elif(abs(Fx1)<EPS):
+            return x1
+        elif(Fx1*Fa<0):
+            return self.bisectionMethod(function,a,x1,EPS)
+        else:
+            return self.bisectionMethod(function,x1,b,EPS)
 
     def findSolution(self,EqFuntion,solFuntion,left,right,step=0.1,EPS=0.00001):
         if(left>right):
@@ -45,15 +65,12 @@ class Solution:
             i+=step
             if(EqFuntion(i)*EqFuntion(i+step)<0):
                 result.append(solFuntion(EqFuntion,i,i+step,EPS))
+            elif(EqFuntion(i)==0):
+                result.append(i)
 
         return result
-
-    def lineEq(self,function,a,b):
-        # find the line equation
-        m = (function(a)-function(b))/(a-b)
-        c = (a*function(a)-b*function(b))/(a-b)
-        return m,c
     
+
 def main():
     eq=Equation()
     sol=Solution()
@@ -63,10 +80,11 @@ def main():
     right=10
     step=0.1
     EPS=0.000000001
+    method=sol.bisectionMethod
 
     for f in eq.functions:
-        print("The roots of the function are: ")
-        result=sol.findSolution(f,sol.falsePostion,left,right,step,EPS)
+        print("The roots of the function by are: ")
+        result=sol.findSolution(f,method,left,right,step,EPS)
         print(result)
 
 
