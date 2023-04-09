fe20e408c8f7425f47bdf55dbb31362b9b0fc90d
Dinlon5566 <ton200168@gmail.com>
2023-04-10 00:45:39 +0800

    Complete Gaussian elimination method

--- Numerical analysis/gaussianElimination.py
+++ Numerical analysis/gaussianElimination.py
@@ -1,10 +1,15 @@
 # Gaussian Elimination
 import random
+import copy
+import sys
 
-def makeArr(minSize=20, maxSize=30):
+debug=False
+
+def makeArr(minSize=20, maxSize=30, n=0, m=0):
     # make n*m array
-    n = random.randint(minSize,maxSize)
-    m = random.randint(minSize, maxSize)
+    if n == 0 and m == 0:
+        n = random.randint(minSize, maxSize)
+        m = n
     arr = [[0 for i in range(m)] for j in range(n)]
     for i in range(n):
         for j in range(m):
@@ -12,59 +17,99 @@ def makeArr(minSize=20, maxSize=30):
     return arr
 
 
-
 def printArr(arr):
+    if arr == None:
+        print("No solution | Infinite solutions")
+        return
+
     for i in range(len(arr)):
         for j in range(len(arr[i])):
             # rounding
             print(round(arr[i][j], 3), end=' ')
         print()
 
-def gaussianElimination(arr):
 
-    n=len(arr)
-    m=len(arr[0])
-    t=min(n, m)
+def gaussianElimination(arr, arrB):
+    n = len(arr)
+    m = len(arr[0])
+    root = [0 for i in range(m)]
+    t = min(n, m)
+
     # forward elimination
     for i in range(t):
         # find max element
-        maxElement=abs(arr[i][i])
-        maxRow=i
+        maxElement = abs(arr[i][i])
+        maxRow = i
         for k in range(i+1, n):
             if abs(arr[k][i]) > maxElement:
-                maxElement=abs(arr[k][i])
-                maxRow=k
+                maxElement = abs(arr[k][i])
+                maxRow = k
 
         # swap max row with current row
         for k in range(i, m):
-            temp=arr[maxRow][k]
-            arr[maxRow][k]=arr[i][k]
-            arr[i][k]=temp
+            temp = arr[maxRow][k]
+            arr[maxRow][k] = arr[i][k]
+            arr[i][k] = temp
+            arrB[maxRow][0], arrB[i][0] = arrB[i][0], arrB[maxRow][0]
 
         # make all rows below this one 0 in current column
         for k in range(i+1, n):
-            c=-arr[k][i]/arr[i][i]
+            c = -arr[k][i]/arr[i][i]
             for j in range(i, m):
-                if i==j:
-                    arr[k][j]=0
+                if i == j:
+                    arr[k][j] = 0
                 else:
-                    arr[k][j]+=c*arr[i][j]
+                    arr[k][j] += c*arr[i][j]
+            arrB[k][0] += c*arrB[i][0]
 
-    return arr
+    # backward substitution
+
+    # if no solution
+    if arr[n-1][m-1] == 0:
+        return None
+
+    # if infinite solutions
+    if arr[n-1][m-1] == 0 and arrB[n-1][0] != 0:
+        return None
+
+    # if one solution
+    root[m-1] = arrB[n-1][0]/arr[n-1][m-1]
+    for i in range(n-2, -1, -1):
+        root[i] = arrB[i][0]
+        for j in range(i+1, m):
+            root[i] -= arr[i][j]*root[j]
+        root[i] /= arr[i][i]
+
+    return root
+
 
 def main():
     # Ax=b
-    arrA=makeArr()
-    arrB=makeArr()
-    print('Size of array: ', len(arrA), 'x', len(arrA[0]))
-    print('A:')
-    printArr(arrA)
+    arrA = makeArr()
+    arrB = makeArr(n=len(arrA), m=1)
 
-    sol=gaussianElimination(arrA)
-    print('Solution:')
-    printArr(sol)
+    arrA_c=copy.deepcopy(arrA)
+    arrB_c=copy.deepcopy(arrB)
+
+    sol = gaussianElimination(arrA_c, arrB_c)
+    
+    if debug:
+        print("A: ")
+        printArr(arrA)
+        print("b: ")
+        printArr(arrB)
+        print("A|: ")
+        printArr(arrA_c)
+        print("|b: ")
+        printArr(arrB_c)
+    print("SOL:")
+    for i in range(len(sol)):
+        print("x"+str(i+1)+"="+str(sol[i]))
 
 
+if __name__ == '__main__':
+    # args = debug mode
+    if len(sys.argv) > 1:
+        debug=True
 
-if __name__ ==  '__main__' :
     main()
